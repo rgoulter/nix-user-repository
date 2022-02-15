d9f11ed133a3472e137578ab116c299e589e0245
Richard Goulter <richard.goulter@gmail.com>
2022-02-15 14:12:55 +0700

    add terraform 0.12.9

--- default.nix
+++ default.nix
@@ -4,5 +4,6 @@
   python-openstackclient = pkgs.callPackage ./pkgs/python-openstackclient { };
   istioctl-1_7_8 = pkgs.callPackage ./pkgs/istioctl/1_7_8 { };
   istioctl-1_8_6 = pkgs.callPackage ./pkgs/istioctl/1_8_6 { };
+  terraform_0_12_9 = pkgs.callPackage ./pkgs/terraform/0_12_9 { };
   myPackages = import ./pkgs/myPackages { pkgs = pkgs; };
 }
---
+++ pkgs/terraform/0_12_9/default.nix
@@ -0,0 +1,7 @@
+{ mkTerraform }:
+
+mkTerraform {
+  version = "0.12.9";
+  sha256 = "sha256-aubq32L3ED460L6zsxgx69LndTrKgpol8GC8jIAlWpI=";
+  patches = [ ../provider-path.patch ];
+}
