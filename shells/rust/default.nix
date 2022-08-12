e0e5df43434489d7efa7c3829e0b20c167e246a8
Richard Goulter <richard.goulter@gmail.com>
2022-08-12 10:09:12 +0700

    shells: rust: add for wasm32-unknown-unknown

--- shells/rust/default.nix
+++ shells/rust/default.nix
@@ -26,4 +26,37 @@
       ];
       RUST_SRC_PATH="${toolchain}/lib/rustlib/src";
     };
+
+  rust_wasm32-unknown-unknown =
+    let
+      target = "wasm32-unknown-unknown";
+      nightly = fenix-pkgs.toolchainOf {
+          channel = "nightly";
+          date = "2022-04-20";
+          sha256 = "sha256-nJgASSAqGkaJ9svoIYVVYrw+YLF4E6AnG02nThtWXDo=";
+        };
+      toolchain = with fenix-pkgs;
+        combine [
+          nightly.defaultToolchain
+          nightly.rust-src
+          (targets.${target}.toolchainOf {
+            channel = "nightly";
+            date = "2022-04-20";
+            sha256 = "sha256-nJgASSAqGkaJ9svoIYVVYrw+YLF4E6AnG02nThtWXDo=";
+          }).rust-std
+        ];
+    in
+    pkgs.mkShell {
+      buildInputs = [
+        pkgs.clang
+        pkgs.pkg-config
+        pkgs.rust-analyzer
+        toolchain
+      ];
+
+      LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+      PROTOC = "${pkgs.protobuf}/bin/protoc";
+      ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
+      RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library/";
+    };
 }
