cd9f42f23c412c68cda4f5913886870de65e97a8
Richard Goulter <richard.goulter@gmail.com>
2022-10-04 10:29:44 +0700

    nixosModules: add module that's gnome-only

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 {
   bluetooth-headset = import ./bluetooth-headset.nix;
   desktops = import ./desktops.nix;
+  desktop-gnome = import ./desktop-gnome.nix;
   keyboards = import ./keyboards.nix;
   printing = import ./printing.nix;
   users = import ./users.nix;
