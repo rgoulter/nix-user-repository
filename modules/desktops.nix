c54f536e5b5268e558e76d686cde7b7e4fcb74f3
Richard Goulter <richard.goulter@gmail.com>
2022-10-06 21:44:53 +0700

    nixosModules: refactor desktops, specialisations gated by enable option

--- modules/desktops.nix
+++ modules/desktops.nix
@@ -1,59 +1,68 @@
 { config, lib, pkgs, ... }:
 
+let
+  cfg = config.rgoulter.desktops;
+in
 {
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-extra
-    # I like using Source Code Pro with Emacs
-    source-code-pro
-  ];
-
-  services = {
-    xserver = {
-      desktopManager.gnome.enable = lib.mkDefault true;
-      displayManager.gdm.enable = lib.mkDefault true;
-
-      enable = true;
-
-      layout = "us";
-    };
+  options.rgoulter.desktops.specialisations = {
+    enable = lib.mkEnableOption "specialisation of other desktop managers";
   };
 
-  specialisation = {
-    gnome.configuration = {
-      services.xserver = {
-        desktopManager.gnome.enable = true;
-        displayManager.gdm.enable = true;
+  config = {
+    fonts.fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-extra
+      # I like using Source Code Pro with Emacs
+      source-code-pro
+    ];
+
+    services = {
+      xserver = {
+        desktopManager.gnome.enable = lib.mkDefault true;
+        displayManager.gdm.enable = lib.mkDefault true;
+
+        enable = true;
+
+        layout = "us";
       };
-      system.nixos.tags = [ "gnome" ];
     };
-    pantheon.configuration = {
-      services.xserver = {
-        # Pantheon conflicts with gnome
-        desktopManager = {
-          gnome.enable = false;
-          pantheon.enable = true;
+
+    specialisation = lib.mkIf cfg.specialisations.enable {
+      gnome.configuration = {
+        services.xserver = {
+          desktopManager.gnome.enable = true;
+          displayManager.gdm.enable = true;
         };
-        # Pantheon requires lightdm
-        displayManager = {
-          gdm.enable = false;
-          lightdm.enable = true;
-        };
+        system.nixos.tags = [ "gnome" ];
       };
-      system.nixos.tags = [ "pantheon" ];
-    };
-    xfce.configuration = {
-      networking.networkmanager.enable = true;
-      services.xserver = {
-        desktopManager = {
-          gnome.enable = false;
-          xfce.enable = true;
+      pantheon.configuration = {
+        services.xserver = {
+          # Pantheon conflicts with gnome
+          desktopManager = {
+            gnome.enable = false;
+            pantheon.enable = true;
+          };
+          # Pantheon requires lightdm
+          displayManager = {
+            gdm.enable = false;
+            lightdm.enable = true;
+          };
         };
-        displayManager = {
-          gdm.enable = false;
-        };
+        system.nixos.tags = [ "pantheon" ];
       };
-      system.nixos.tags = [ "gnome" ];
+      xfce.configuration = {
+        networking.networkmanager.enable = true;
+        services.xserver = {
+          desktopManager = {
+            gnome.enable = false;
+            xfce.enable = true;
+          };
+          displayManager = {
+            gdm.enable = false;
+          };
+        };
+        system.nixos.tags = [ "gnome" ];
+      };
     };
   };
 }
