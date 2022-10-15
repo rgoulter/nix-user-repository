7be4cff522693655226064ea73f22318b5aa3927
Richard Goulter <richard.goulter@gmail.com>
2022-10-15 18:15:51 +0700

    shells: fix haskell shell

--- shells/haskell/default.nix
+++ shells/haskell/default.nix
@@ -1,11 +1,9 @@
 {pkgs ? import <nixpkgs> {}}: {
   haskell = pkgs.mkShell {
     packages = with pkgs; [
-      cabal-fmt
       ghcid
       haskell-language-server
       ormolu
     ];
   };
 }
-
