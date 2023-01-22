576aba12a709891510519897b984c945d03cf5eb
Richard Goulter <richard.goulter@gmail.com>
2023-01-22 10:20:34 +0700

    modules: tailscale: add the tailscale-up-auto systemd service

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -3,14 +3,37 @@
   lib,
   pkgs,
   ...
-}: {
-  environment.systemPackages = [pkgs.tailscale];
-  networking.firewall = {
-    trustedInterfaces = ["tailscale0"];
-    allowedUDPPorts = [config.services.tailscale.port];
-    allowedTCPPorts = [22];
+}: let
+  cfg = config.rgoulter.tailscale;
+in {
+  options = {
+    rgoulter.tailscale.enable-tailscale-up-auto = lib.mkEnableOption "tailscale up auto";
   };
-  services = {
-    tailscale.enable = true;
+  config = {
+    environment.systemPackages = [pkgs.tailscale];
+    networking.firewall = {
+      trustedInterfaces = ["tailscale0"];
+      allowedUDPPorts = [config.services.tailscale.port];
+      allowedTCPPorts = [22];
+    };
+    services = {
+      tailscale.enable = true;
+    };
+    systemd.services.tailscale-up-auto = lib.mkIf cfg.enable-tailscale-up-auto {
+      description = "Automatic connection to Tailscale";
+      after = ["network-pre.target" "tailscale.service"];
+      wants = ["network-pre.target" "tailscale.service"];
+      wantedBy = ["multi-user.target"];
+      serviceConfig.Type = "oneshot";
+      path = with pkgs; [jq tailscale];
+      script = ''
+        sleep 2
+        status="$(tailscale status -json | jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
+        tailscale up --authkey=file:/run/keys/auth-key
+      '';
+    };
   };
 }
