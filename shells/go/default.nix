124abca185fdf16b87c67425f6f2c3c82c09f044
Richard Goulter <richard.goulter@gmail.com>
2023-02-18 14:22:39 +0700

    shells: go: remove go_1_17 (no longer available)

--- shells/go/default.nix
+++ shells/go/default.nix
@@ -5,12 +5,6 @@
   go = pkgs.mkShell {
     inherit (languages.go) packages;
   };
-  go_1_17 = pkgs.mkShell {
-    packages = with pkgs; [
-      go_1_17
-      gopls
-    ];
-  };
   go_1_18 = pkgs.mkShell {
     packages = with pkgs; [
       go_1_18
