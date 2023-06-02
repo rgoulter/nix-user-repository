95968968d32f7a6754978f099e975ca56b916977
Richard Goulter <richard.goulter@gmail.com>
2023-06-02 12:27:26 +0700

    nixos modules: bluetooth-headset: update for 23.05

--- modules/bluetooth-headset.nix
+++ modules/bluetooth-headset.nix
@@ -4,6 +4,17 @@
   pkgs,
   ...
 }: {
+  # https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
+  environment.etc = {
+  	"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  		bluez_monitor.properties = {
+  			["bluez5.enable-sbc-xq"] = true,
+  			["bluez5.enable-msbc"] = true,
+  			["bluez5.enable-hw-volume"] = true,
+  			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  		}
+  	'';
+  };
   # https://nixos.wiki/wiki/Bluetooth#Using_Bluetooth_headsets_with_PulseAudio
   # https://nixos.wiki/wiki/Bluetooth#Enabling_A2DP_Sink
   hardware = {
@@ -28,36 +39,6 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      media-session.config.bluez-monitor.rules = [
-        {
-          # Matches all cards
-          matches = [{"device.name" = "~bluez_card.*";}];
-          actions = {
-            "update-props" = {
-              "bluez5.reconnect-profiles" = ["hfp_hf" "hsp_hs" "a2dp_sink"];
-              # mSBC: higher quality in calls.
-              # mSBC is not expected to work on all headset + adapter combinations.
-              "bluez5.msbc-support" = true;
-              # SBC-XQ: higher audio
-              # SBC-XQ is not expected to work on all headset + adapter combinations.
-              "bluez5.sbc-xq-support" = true;
-
-              "bluez5.autoswitch-profile" = true;
-            };
-          };
-        }
-        {
-          matches = [
-            # Matches all sources
-            {"node.name" = "~bluez_input.*";}
-            # Matches all outputs
-            {"node.name" = "~bluez_output.*";}
-          ];
-          actions = {
-            "node.pause-on-idle" = false;
-          };
-        }
-      ];
     };
   };
 }
