154eff011359c2ec803fda3d8ccc5fe2ae12c8bc
Richard Goulter <richard.goulter@gmail.com>
2023-09-22 14:19:17 +0700

    pkgs/terraform: fix argument

--- pkgs/terraform/0_12_9/default.nix
+++ pkgs/terraform/0_12_9/default.nix
@@ -1,6 +1,6 @@
 {mkTerraform}:
 mkTerraform {
   version = "0.12.9";
-  sha256 = "sha256-aubq32L3ED460L6zsxgx69LndTrKgpol8GC8jIAlWpI=";
+  hash = "sha256-aubq32L3ED460L6zsxgx69LndTrKgpol8GC8jIAlWpI=";
   patches = [../provider-path.patch];
 }
