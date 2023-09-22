ee7d823b6f6a9f58e435715c000a5f9ef49c1eca
Richard Goulter <richard.goulter@gmail.com>
2023-09-22 12:38:41 +0700

    nixos modules: keyboards: use hardware.keyboard.qmk

--- modules/keyboards.nix
+++ modules/keyboards.nix
@@ -8,20 +8,8 @@
 # keyboards, # and using stlink (to flash STM MCUs)
 # without sudo.
 {
-  services.udev = {
-    # https://docs.qmk.fm/#/faq_build?id=linux-udev-rules
-    extraRules = ''
-      # Atmel DFU
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", TAG+="uaccess"
-
-      # STM32 DFU
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", TAG+="uaccess"
-
-      # STM32duino 1eaf:0003
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1eaf", ATTRS{idProduct}=="0003", TAG+="uaccess"
-    '';
-    packages = [
-      pkgs.stlink
-    ];
-  };
+  hardware.keyboard.qmk.enable = true;
+  services.udev.packages = [
+    pkgs.stlink
+  ];
 }
