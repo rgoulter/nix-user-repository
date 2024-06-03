b104f4b77152d0ed07b67a16207baecb4de7d6d1
Richard Goulter <richard.goulter@gmail.com>
2024-06-03 20:04:54 +0700

    pkgs: remove old terraform packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,4 @@
 {pkgs}: {
   python-openstackclient = pkgs.callPackage ./python-openstackclient {};
   istioctl-1_11_0 = pkgs.callPackage ./istioctl/1_11_0 {};
-  terraform_0_12_9 = pkgs.callPackage ./terraform/0_12_9 {};
-  terraform_0_12_31 = pkgs.callPackage ./terraform/0_12_31 {};
 }
--- pkgs/terraform/0_12_31/default.nix
+++
@@ -1,6 +0,0 @@
-{mkTerraform}:
-mkTerraform {
-  version = "0.12.31";
-  hash = "sha256-z50WYdLz/bOhcMT7hkgaz35y2nVho50ckK/M1TpK5g4=";
-  patches = [../provider-path.patch];
-}
