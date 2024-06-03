07a23cfb735ee0d89a3885762f2c1ec603d58fe0
Richard Goulter <richard.goulter@gmail.com>
2024-06-03 20:13:21 +0700

    shells: python: remove 3.8, 3.9

--- shells/python/default.nix
+++ shells/python/default.nix
@@ -2,27 +2,6 @@
   pkgs ? import <nixpkgs> {},
   languages,
 }: {
-  python_3_8 = pkgs.mkShell {
-    packages = with pkgs; [
-      libffi
-      python38
-      pyright
-    ];
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-    ];
-  };
-  python_3_9 = pkgs.mkShell {
-    packages = with pkgs; [
-      libffi
-      python39
-      python39.pkgs.python-lsp-server
-      pyright
-    ];
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-    ];
-  };
   python_3_10 = pkgs.mkShell {
     inherit (languages.python) packages;
     inherit (languages.python.environment) LD_LIBRARY_PATH;
