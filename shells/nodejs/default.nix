49be6f89179d719a891ec52c749442d734135423
Richard Goulter <richard.goulter@gmail.com>
2024-06-03 20:12:09 +0700

    shells: nodejs: remove nodejs 16, 18

--- shells/nodejs/default.nix
+++ shells/nodejs/default.nix
@@ -5,22 +5,4 @@
   nodejs = pkgs.mkShell {
     inherit (languages.nodejs) packages;
   };
-  nodejs-16 = pkgs.mkShell {
-    packages = with pkgs; [
-      nodejs-16_x
-      nodePackages.eslint
-      nodePackages.typescript
-      nodePackages.typescript-language-server
-      yarn
-    ];
-  };
-  nodejs-18 = pkgs.mkShell {
-    packages = with pkgs; [
-      nodejs-18_x
-      nodePackages.eslint
-      nodePackages.typescript
-      nodePackages.typescript-language-server
-      yarn
-    ];
-  };
 }
